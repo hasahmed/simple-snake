f1648304d150f8d6352ded4d9cb4205d084c240b
Hasan Y Ahmed <hasahmed@umail.iu.edu>
2017-09-26 22:38:17 -0400

    making sure everything is added

--- build.rb
+++ build.rb
@@ -1,14 +1,19 @@
+#!/usr/bin/ruby
 def main(cp)
     if ARGV.length == 0
-        system("javac *.java")
+        system("javac -cp #{cp} *.java")
         return
     end
     if ARGV.include?("compile")
-        system("javac *.java")
+        system("javac -cp #{cp} *.java")
     end
     if ARGV.include?("jar")
         system("jar cfm snake.jar Manifest.txt *.class")
         system("rm *.class")
     end
+    if ARGV.include?("run")
+        system("java -cp #{cp} Snake")
+    end
 end
-main("hey")
+classpath = "./jasypt-1.9.2.jar:."
+main(classpath)
